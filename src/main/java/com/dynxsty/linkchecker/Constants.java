8ede1d99169f834a432995157fde6bf22d3066a1
Dynxsty <jasonlessenich@gmail.com>
2021-08-29 13:21:51 +0200

    improved logging

--- src/main/java/com/dynxsty/linkchecker/Constants.java
+++ src/main/java/com/dynxsty/linkchecker/Constants.java
@@ -4,6 +4,9 @@ import java.awt.*;
 
 public class Constants {
 
+    public static final String TEXT_RESET = "\u001B[0m";
+    public static final String TEXT_CYAN = "\u001B[36m";
+
     public static final Color EMBED_GRAY = Color.decode("#2F3136");
     public static final String CONFIG_PATH = "config.properties";
 }
