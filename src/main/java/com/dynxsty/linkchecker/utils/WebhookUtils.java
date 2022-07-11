0ce8ef1d094863d589f8f19445bb46e1d980e678
Dynxsty <jasonlessenich@gmail.com>
2022-07-12 00:07:59 +0200

    optimized imports

--- src/main/java/com/dynxsty/linkchecker/utils/WebhookUtils.java
+++ src/main/java/com/dynxsty/linkchecker/utils/WebhookUtils.java
@@ -7,8 +7,6 @@ import com.dynxsty.linkchecker.Bot;
 import com.dynxsty.linkchecker.data.SystemsConfig;
 import lombok.extern.slf4j.Slf4j;
 
-import java.util.concurrent.CompletableFuture;
-
 @Slf4j
 public class WebhookUtils {
 	private WebhookUtils() {
